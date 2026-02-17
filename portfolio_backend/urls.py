56e592ca1926ce435148e50322f618cee4c17a83
Dhruv Vaishnav <dhruv2vaishnav@gmail.com>
2026-02-17 11:49:09 +0530

    urls.py edited

--- portfolio_backend/urls.py
+++ portfolio_backend/urls.py
@@ -1,23 +1,22 @@
 """
 URL configuration for portfolio_backend project.
-
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+ https://docs.djangoproject.com/en/5.2/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from . import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+ 1. Add an import: from . import views
+ 2. Add a URL to urlpatterns: path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app import views
-    2. Add a URL to urlpatterns:  path('', views.Home.as_view(), name='home')
+ 1. Add an import: from other_app import views
+ 2. Add a URL to urlpatterns: path('', views.Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+ 1. Import the include() function: from django.urls import include, path
+ 2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  # ✅ FIXED: Added this import
-from django.conf.urls.static import static  # ✅ FIXED: Added this import
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Admin panel
@@ -27,7 +26,6 @@ urlpatterns = [
     path('', include('api.urls')),
 ]
 
-# ✅ FIXED: This is the ONLY way Django serves CSS/JS during development
-# This configuration tells Django where to find your static files (CSS, JS, images)
+# ✅ FIXED: Serve static files from STATIC_ROOT during development
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
